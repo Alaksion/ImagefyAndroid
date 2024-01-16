0d3ce3f2ab018dccb0b81a8f127095cb651c448c
Lucca Beurmann <lbeurmann.dev@gmail.com>
2024-01-16 02:03:46 -0300

    chore: sample code

--- app/src/main/java/io/github/alaksion/imagefy/MainActivity.kt
+++ app/src/main/java/io/github/alaksion/imagefy/MainActivity.kt
@@ -4,15 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.alaksion.imagefy.ui.theme.ImagefyTheme
 import io.github.alaksion.unsplashwrapper.sdk.UnsplashWrapperSdk
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,23 +22,28 @@ class MainActivity : ComponentActivity() {
         setContent {
             val sdk = UnsplashWrapperSdk.Instance
             sdk.initialize("smXXBhupy1xAZ6npy5NjO7XGmJ0fjuvLnQfaz_UwWlY")
+            val scope = rememberCoroutineScope()
 
             ImagefyTheme {
-                LaunchedEffect(key1 = Unit) {
-                    try {
-                        sdk.photosRepository.getPhotos().map {
-                            println(it.description)
-                        }
-                    } catch (e: Throwable) {
-                        println(e)
-                    }
-                }
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
+                    Button(onClick = {
+                        scope.launch {
+                            try {
+                                sdk.photosRepository.getPhotos().map {
+                                    println(it.description)
+                                }
+                            } catch (e: Throwable) {
+                                println(e)
+                            }
+                        }
+                    }) {
+                        Text("send request")
+                    }
                 }
             }
         }
