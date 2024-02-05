d1a10dbb37b47594779480e87d34ab0ad2debb76
Lucca Beurmann <lbeurmann.dev@gmail.com>
2024-02-04 23:55:38 -0300

    feat: add immutable collections dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation("io.github.alaksion:unsplash-wrapper:0.0.1")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.7")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
