5591e074c4fd9cde3b6854a61314ec5a2648145c
Lucca Beurmann <lbeurmann.dev@gmail.com>
2024-02-16 22:49:53 -0300

    feat: add stability report configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,3 +5,29 @@ plugins {
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.jetbrains.compose) apply false
 }
+
+
+/*
+* run to print stability report:
+*
+* ./gradlew assembleRelease -PcomposeCompilerReports=true --rerun-tasks
+*
+* */
+subprojects {
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions {
+            if (project.findProperty("composeCompilerReports") == "true") {
+                freeCompilerArgs += listOf(
+                    "-P",
+                    "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=${project.buildDir.absolutePath}/compose_compiler"
+                )
+            }
+            if (project.findProperty("composeCompilerMetrics") == "true") {
+                freeCompilerArgs += listOf(
+                    "-P",
+                    "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=${project.buildDir.absolutePath}/compose_compiler"
+                )
+            }
+        }
+    }
+}
