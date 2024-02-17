da9fa407ea95aeb70e8cb75908312818e303e9a4
Lucca Beurmann <lbeurmann.dev@gmail.com>
2024-02-16 22:08:56 -0300

    feat: add test dependency to screen model

--- platform/state-screenmodel/build.gradle.kts
+++ platform/state-screenmodel/build.gradle.kts
@@ -6,5 +6,6 @@ plugins {
 dependencies {
     implementation(libs.voyager.screenmodel)
     implementation(libs.coroutines.core)
+    testImplementation(kotlin("test"))
     testImplementation(libs.coroutines.test)
 }
