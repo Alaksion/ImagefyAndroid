0bba68f0ab65d53a89cd786a95cc133839f02102
Lucca Beurmann <lbeurmann.dev@gmail.com>
2024-02-16 22:12:29 -0300

    feat: audhawd

--- app/src/main/java/io/github/alaksion/imagefy/features/home/HomeScreen.kt
+++ app/src/main/java/io/github/alaksion/imagefy/features/home/HomeScreen.kt
@@ -2,18 +2,30 @@ package io.github.alaksion.imagefy.features.home
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.kodein.rememberScreenModel
 
 internal class HomeScreen : Screen {
 
     @Composable
     override fun Content() {
-        HomeScreenContent()
+        val model: HomeScreenModel = rememberScreenModel()
+        val state by model.state.collectAsState()
+        LaunchedEffect(key1 = model) { model.initialize() }
+
+        HomeScreenContent(
+            state = state.data
+        )
     }
 
 }
 
 @Composable
-private fun HomeScreenContent() {
+private fun HomeScreenContent(
+    state: HomeState
+) {
     Text("hello world")
 }
